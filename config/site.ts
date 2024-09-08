0a2f3a41183bdada0adbdca4640817baf0868d33
itsroge <rogeriov.dsg@gmail.com>
2024-09-08 12:51:12 -0300

    cleaning some secondary stuff
    changed /pessoas to /comunidade and (trying to) clean some other stuff

--- config/site.ts
+++ config/site.ts
@@ -3,17 +3,14 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   mainNav: [
     {
-      title: "Diretório",
+      title: "Comunidade",
       href: "/pessoas",
     },
-    {
-      title: "FAQ",
-      href: "/perguntas",
-    },
+
   ],
   links: {
     notx: "https://handles.notx.blue",
-    bluesky: "https://bsky.app/profile/rogeriov.bsky.social",
     github: "https://github.com/itsroge/emos.social",
   },
 }
+
